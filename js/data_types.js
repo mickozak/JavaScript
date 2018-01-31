ff762b462090571e86ccb01a09cb13e4d5919861
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-01-31 11:07:04 +0100

    Create data_types.js

---
+++ js/data_types.js
@@ -0,0 +1,15 @@
+There are two types of data: primitive and reference.
+
+Primitive data:
+
+- number e.g. 5
+- string e.g. 'Car'
+- boolean e.g. true or falsh
+- undefined
+- null
+- symbol
+
+Reference data:
+
+- objects
+- array
