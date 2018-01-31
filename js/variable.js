14568a58f4823c62ae9ef82e68ccba3f5fbd5c0b
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-01-31 11:35:01 +0100

    Update variable.js

--- js/variable.js
+++ js/variable.js
@@ -1,3 +1,18 @@
+//KEYWORDS
+
+var //old
+let //ES6
+const //ES6
+
+/*NAMING
+
+- variable name can not start with a digit,
+- variable name can not contain spaces,
+- variable name can not be a keyword.
+
+Use camel notation.*/
+
+
 //VARIABLE DECLARATION
 
 let firstVariable;
