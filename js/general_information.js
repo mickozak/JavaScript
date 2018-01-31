5828c0221dc154e3b30342e14fb4223a1c38476a
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-01-31 13:47:22 +0100

    Create general_information.js

---
+++ js/general_information.js
@@ -0,0 +1,15 @@
+//Script on the site.
+
+To place the scripts on the page, use the <script> tag.
+
+<script>
+        let firstName = 'Sara';
+</script>
+
+//Script in a separate file.
+
+We place the script in the "head" section.
+
+<script src="name.js"></script>
+
+You can attach two attributes: async and defer.
