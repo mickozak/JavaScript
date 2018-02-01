528d3b39f7ff3edc6415994bf9172b8ad5b2db52
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-01 18:05:29 +0100

    Update conditional_instructions.js

--- js/conditional_instructions.js
+++ js/conditional_instructions.js
@@ -1,3 +1,26 @@
 //If
 
 //General form: if (condition) {statement}
+
+//e.g. 1
+
+let x = 2;
+
+if (x == 2){
+    alert('x=2');
+} else {
+    alert('x!=2')
+}
+    
+//e.g. 2
+
+let answer = prompt('How much money do you want to spend for dinner?');
+
+if (answer<=10){
+    alert('You must stay at home.');
+} else if (answer<=30){
+    alert('You can go to the bar.');
+} else {
+    alert('You can go to the restaurant.')
+}
+    
