91dd849bbb6d22b520e5350c7359c8b6c6929a57
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-02 22:39:07 +0100

    Update loops.js

--- js/loops.js
+++ js/loops.js
@@ -1,1 +1,16 @@
+//The loop allows you to execute the code a certain number of times.
 
+//for
+
+for (let x=0; x<100; x++){
+    console.log('I will go to the gym on Monday');
+}
+
+//while
+
+let i = 1;
+
+while (i <=50) {
+    console.log('I will go...');
+    i++;
+}
