0498473c57bee4578689c36d4c9c680da644a873
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-03 17:29:06 +0100

    Create functions_callback_closures.js

---
+++ js/functions_callback_closures.js
@@ -0,0 +1,7 @@
+//A function is a set of expressions that perform a task or calculate a value. To use a function, you must first define it somewhere in the range from which it will be called.
+
+//Declaration of functions
+
+function sum (a,b) {
+	.......
+}
