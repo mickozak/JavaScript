d93f578d3f2c736209df36f2df90969cac356d91
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-06 19:04:16 +0100

    Create object.js

---
+++ js/object.js
@@ -0,0 +1,9 @@
+//The object has properties and methods. The object has no index. We refer to the object through properties.
+
+let myObj = {
+  name: "Sara",
+  age: 27,
+  print: function(){
+    console.log(this.name)
+    }
+ }
