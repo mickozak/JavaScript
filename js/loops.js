f0eaa25bf4d5666240350117272a39008d173cc4
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-13 22:01:01 +0100

    Update loops.js

--- js/loops.js
+++ js/loops.js
@@ -1,22 +1,22 @@
 //The loop allows you to execute the code a certain number of times.
 
-//for
+//The loop "for" - repeats the statement execution until the condition provided returns false.
 
-//e.g.1
+//e.g. 1
 
 for (let x=0; x<100; x++){
     console.log('I will go to the gym on Monday');
 }
 
-//e.g.2
+//e.g. 2
 
 for (let i=0; i<=100; i++){
 console.log(i)
 }
 
-//while 
+//The loop "while" - is performed as long as the condition returns true.
 
-//e.g.3
+//e.g. 3
 
 let i = 1;
 
@@ -25,10 +25,10 @@ while (i <=50) {
     i++;
 }
 
-//The loop forEach - it is useful when, for example, you have an array with some elements and
+//The loop "forEach" - it is useful when, for example, you have an array with some elements and
 //you want to do something for each of these elements.
 
-//e.g.4
+//e.g. 4
 
 let car = ['Audi','Bmw', 'Fiat', 'Toyota'];
 
@@ -37,9 +37,9 @@ car.forEach(function(elements,index,array){
     console.log(index + '. ' + elements);
 });
 
-//The loop do...while- repeats the instructions inside until the condition returns false.
+//The loop "do...while" - repeats the instructions inside until the condition returns false.
 
-//e.g.5
+//e.g. 5
 
 do {
   i += 1;
