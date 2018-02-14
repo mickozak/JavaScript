b54d14508b9695f9587e587b04724760bd84fe33
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-14 22:37:53 +0100

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,6 @@
 //A simple quiz containing general questions and counting the result.
+
+let score = 0;
+   
+let questionQuiz = [['In which year began the first world war?',1939],['How many degrees has a right angle?',90],['How many planets are in the solar system?',8]];
+
