733a4f91729d2483d9162a34a9df8aefcd9442ce
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-14 22:40:15 +0100

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -4,3 +4,14 @@ let score = 0;
    
 let questionQuiz = [['In which year began the first world war?',1939],['How many degrees has a right angle?',90],['How many planets are in the solar system?',8]];
 
+function askQuestion (questionQuiz){
+   
+    let answer = prompt(questionQuiz[0],'');
+    
+    if (answer==questionQuiz[1]){
+        alert('Well done!');
+        score++;
+    } else {
+        alert('The wrong answer. ' + 'The correct answer is: ' + questionQuiz[1] + ' .');
+    }
+}
