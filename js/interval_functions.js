59ece2ab530a16c52bbf4aa91cb02b59747e2619
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-19 20:34:02 +0100

    Update interval_functions.js

--- js/interval_functions.js
+++ js/interval_functions.js
@@ -1,5 +1,11 @@
 //setTimeout() - we run the code with a time delay.
 
+function mySetTimeout (){
+    console.log('Hello!');
+}
+
+setTimeout(mySetTimeout,3000);
+
 //clearTimeout() - interrupts setTimeout().
 
 //setInterval() - we run the code cyclically.
