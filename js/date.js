c8aec5585c559463e9a59c872f98c12f9556ec35
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-21 22:18:24 +0100

    Update date.js

--- js/date.js
+++ js/date.js
@@ -3,3 +3,9 @@
 let currentDate = new Date();
     
 console.log(currentDate);
+
+//For example:
+    
+let element = document.querySelector('#divOneTest');
+element.innerHTML='It is: ' + currentDate.getHours() + ':' + currentDate.getMinutes();
+    
