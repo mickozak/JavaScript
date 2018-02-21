14def23693ca8e2bf3b75107a20c99c7c7c67f4c
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-21 22:18:50 +0100

    Update date.js

--- js/date.js
+++ js/date.js
@@ -8,4 +8,9 @@ console.log(currentDate);
     
 let element = document.querySelector('#divOneTest');
 element.innerHTML='It is: ' + currentDate.getHours() + ':' + currentDate.getMinutes();
+
+//For example:
     
+function showZero (i){
+    return (i<10) ? '0' + i : i;
+}
