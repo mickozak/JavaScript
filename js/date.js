2199b06808e938c4f252e1ebdc942c41ddccc618
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-21 22:19:03 +0100

    Update date.js

--- js/date.js
+++ js/date.js
@@ -14,3 +14,6 @@ element.innerHTML='It is: ' + currentDate.getHours() + ':' + currentDate.getMinu
 function showZero (i){
     return (i<10) ? '0' + i : i;
 }
+
+let elementTwo = document.querySelector('#divTwoTest');
+elementTwo.innerHTML='Today is: ' + showZero(currentDate.getDate()) + '.' + showZero(currentDate.getMonth()+1) + '.' + currentDate.getFullYear();
