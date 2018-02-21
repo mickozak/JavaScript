31874dd47199d4f3f9331292f9cba4c542c77f0d
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-21 22:19:37 +0100

    Update date.js

--- js/date.js
+++ js/date.js
@@ -7,13 +7,25 @@ console.log(currentDate);
 //For example:
     
 let element = document.querySelector('#divOneTest');
+
 element.innerHTML='It is: ' + currentDate.getHours() + ':' + currentDate.getMinutes();
 
 //For example:
     
 function showZero (i){
+    
     return (i<10) ? '0' + i : i;
+    
 }
 
 let elementTwo = document.querySelector('#divTwoTest');
+
 elementTwo.innerHTML='Today is: ' + showZero(currentDate.getDate()) + '.' + showZero(currentDate.getMonth()+1) + '.' + currentDate.getFullYear();
+
+//For example:
+    
+let arrayWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+let elementThree = document.querySelector('#divThreeTest');
+    
+elementThree.innerHTML='Today is: ' + arrayWeek[currentDate.getDay()];
