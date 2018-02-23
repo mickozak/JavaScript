fb24b242ac390c29907f359c4488c457ec8740f4
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-23 21:23:20 +0100

    Update math.js

--- js/math.js
+++ js/math.js
@@ -1,1 +1,22 @@
+//The facility allows performing mathematical operations.
+
+//For example:
+
+let one = 10;
+
+let two = 2;
+
+let three = 30;
+
+console.log(Math.max(one,two,three));
+
+console.log(Math.min(one,two,three));
+
+console.log(Math.max(4,3,7,5,1));
+
+console.log(Math.round(3,23));
+
+console.log(Math.floor(2,34));
+
+console.log(Math.ceil(7,65));
 
