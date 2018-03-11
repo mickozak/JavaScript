69893aeeeef938dcbe5bf486e43e0dadd081a219
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-03-10 21:31:54 -1200

    Update this.js

--- js/this.js
+++ js/this.js
@@ -1,2 +1,16 @@
 //Key word this does not refer to the current function nor is a reference to the lexical scope of this function.
 //This is a binding that arises when calling a function.
+
+//Types of bonds:
+
+//#1
+
+function one() {
+    console.log(this.variableOne);
+}
+    
+var variableOne = 2;
+    
+one();
+
+
