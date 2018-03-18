3a870fe6e34b50b1b34134b8553d14c803ce739b
Michał Kozak <kozak_m@outlook.com>
2018-03-18 11:01:18 +0100

    Update general_information.js

--- js/general_information/general_information.js
+++ js/general_information/general_information.js
@@ -1,17 +1,19 @@
-//JavaScript - script programming language, created by Netscape, most commonly used on websites.
+/*
+JavaScript - script programming language, created by Netscape, most commonly used on websites.
 
-//Script on the site.
+Script on the site.
 
-//To place the scripts on the page, use the <script> tag.
+To place the scripts on the page, use the <script> tag.
 
 <script>
         let firstName = 'Sara';
 </script>
 
-//Script in a separate file.
+Script in a separate file.
 
-//We place the script in the "head" section.
+We place the script in the "head" section.
 
 <script src="general_information.js"></script>
 
-//You can attach two attributes: async and defer.
+You can attach two attributes: async and defer.
+*/
