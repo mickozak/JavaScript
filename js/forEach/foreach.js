4f58ccf66d27e8713b454c8b752191478514694f
Michał Kozak <kozak_m@outlook.com>
2018-04-14 18:44:57 +0200

    Create foreach.js

---
+++ js/forEach/foreach.js
@@ -0,0 +1,8 @@
+
+//The forEach() method executes a provided function once for each array element.
+
+var newArray = [1,2,3,4,5,6,7,8,9]
+
+newArray.forEach(function (value){
+    return console.log(value+1);
+})
