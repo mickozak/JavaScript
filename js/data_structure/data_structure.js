1c497ce711a8221ba8a56066edb179941c030258
Michał Kozak <kozak_m@outlook.com>
2018-04-14 18:56:13 +0200

    Create data_structure.js

---
+++ js/data_structure/data_structure.js
@@ -0,0 +1,54 @@
+//The forEach() method executes a provided function once for each array element.
+
+var newArray = [1,2,3,4,5,6,7,8,9];
+
+newArray.forEach(function (value){
+    return console.log(value+1);
+})
+
+//Unshift & push
+
+var arrayOne = [1,2,3,4,5,6,7,8,9]
+
+arrayOne.unshift(0);
+arrayOne.push(10);
+
+console.log(arrayOne);
+
+//Shift & pop
+
+arrayOne.shift();
+arrayOne.pop();
+
+console.log(arrayOne);
+
+//Concat
+
+var arrayTwo = [10,11,12,13,14,15];
+var arrayThree = arrayOne.concat(arrayTwo);
+
+console.log(arrayThree);
+
+//Splice
+
+var four = arrayThree.splice(3,7);
+
+console.log(four);
+
+//indexOf
+
+var five = arrayThree.indexOf(1);
+
+console.log(five);
+
+//slice
+
+var six = arrayThree.slice(2,5);
+
+console.log(six);
+
+//join
+
+var seven = arrayThree.join(',');
+
+console.log(seven);
