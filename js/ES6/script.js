6d253c03d10931e7efdcd30e23a1035208e708b1
Michał Kozak <kozak_m@outlook.com>
2018-05-02 11:03:30 +0200

    Update script.js

--- js/ES6/script.js
+++ js/ES6/script.js
@@ -39,3 +39,48 @@ var name = 'Michael'
 var age = 25
 
 console.log(`My name is ${name}. I am ${age} years old`);
+
+//DECOMPOSITION
+
+var objOne = {
+    name: 'Michael',
+    surname: 'Nowak'
+}
+
+const {name, surname} = objOne
+
+console.log(name)
+console.log(surname)
+
+const tab = [1, 2, 3];
+
+[a, b, c] = tab;
+
+console.log(a, b, c)
+
+//CLASS
+
+class Animal {
+    constructor(name){
+        this.name=name;
+    }
+    eat(){
+        return 'I eat food';
+    }
+}
+
+const objAnimal = new Animal('Tiger')
+console.log(objAnimal)
+
+class Cats extends Animal{
+    constructor(name){
+        super(name);
+        this.type='cat'
+    }
+    run(){
+        return 'I can run'
+    }
+}
+
+const objCat = new Cats('Max')
+console.log(objCat)
